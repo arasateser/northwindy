fcdc676210278c40c14112619d0b581b54b99b5b
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-07-15 16:11:59 +0300

    visual fixes

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product',
@@ -6,17 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  product1 = {productId:1, productName:'Bardak', categoryId:1, unitPrice:5}
-  product2 = {productId:2, productName:'Tabak', categoryId:1, unitPrice:5}
-  product3 = {productId:3, productName:'Canak', categoryId:1, unitPrice:5}
-  product4 = {productId:4, productName:'Kasik', categoryId:1, unitPrice:5}
-  product5 = {productId:5, productName:'Bicak', categoryId:1, unitPrice:5}
-
-  products=[
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5
-  ]
+  products:Product[]=[  ]
 }
