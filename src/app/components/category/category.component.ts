df7cb903891242f9e3eb21d7684ef31bcf1f9ecc
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-07-16 14:57:21 +0300

    adding and refactoring category component

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../../models/category';
+import { HttpClient } from '@angular/common/http';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
-export class CategoryComponent {
 
+export class CategoryComponent implements OnInit {
+  categories : Category[]=[];
+
+  constructor(private categoryService:CategoryService){ }
+
+  ngOnInit(): void {
+    this.getCategory();
+  }
+
+  getCategory(){
+    this.categoryService
+    .getCategories()
+    .subscribe(response => {
+      this.categories = response.data
+    });
+  }
 }
