32f0ec588302a746a35fa7780209fe59dc6612ae
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-07-16 19:21:13 +0300

    category routing

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../../models/product';
 import { HttpClient } from '@angular/common/http';
 import { ProductService } from '../../services/product.service';
 import { response } from 'express';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -13,15 +14,30 @@ export class ProductComponent implements OnInit {
   products:Product[]=[]
   dataLoaded = false;
 
-  constructor(private productService:ProductService){ }
+  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute){ }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }else{
+        this.getProducts()
+      }
+    })
   }
 
-  getProducts(){
+  getProducts() {
     this.productService
     .getProducts()
+    .subscribe((response)=>{
+      this.products = response.data;
+      this.dataLoaded = true;
+    })
+  }
+
+    getProductsByCategory(categoryId:number) {
+    this.productService
+    .getProductsByCategory(categoryId)
     .subscribe((response)=>{
       this.products = response.data;
       this.dataLoaded = true;
