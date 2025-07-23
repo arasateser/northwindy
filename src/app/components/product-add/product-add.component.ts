040ade872dab436f456ef82f899b518ae6be8fdb
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-07-23 17:38:36 +0300

    validation error for form

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from '../../services/product.service';
+import { ToastrService } from 'ngx-toastr';
+import { response } from 'express';
 
 @Component({
   selector: 'app-product-add',
@@ -10,13 +13,31 @@ export class ProductAddComponent {
 
   productAddForm:FormGroup;
 
-  constructor(private formBuilder:FormBuilder){
+  constructor(private formBuilder:FormBuilder,
+              private productService:ProductService,
+              private toastrService:ToastrService){
     this.productAddForm=formBuilder.group({
       productName:["",Validators.required],
       unitPrice:["",Validators.required],
       unitsInStock:["",Validators.required],
       categoryId:["",Validators.required]
     })
+  }
+
+  add() {
+    if(this.productAddForm.valid){
+      let productModel = Object.assign({},this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response=>{
+        this.toastrService.success("yessir", "SUCCESS")
+      },responseError=>{
+        if(responseError.error.Errors.length>0){
+          for(let i=0; i < responseError.error.Errors.length;i++)
+          this.toastrService.error(responseError.error.Errors[i].ErrorMessage,"valdiation error sir")
+        }
+      })
+    }else{
+      this.toastrService.error("form element(s) missing","ERROR")
+    }
   }
 
 }
