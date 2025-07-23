0ed5c4ba16df3e8cb0e97aabaeb89ae78d5f4a19
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-07-23 13:21:04 +0300

    Product add form

---
+++ src/app/components/product-add/product-add.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-product-add',
+  templateUrl: './product-add.component.html',
+  styleUrl: './product-add.component.css'
+})
+export class ProductAddComponent {
+
+  productAddForm:FormGroup;
+
+  constructor(private formBuilder:FormBuilder){
+    this.productAddForm=formBuilder.group({
+      productName:["",Validators.required],
+      unitPrice:["",Validators.required],
+      unitsInStock:["",Validators.required],
+      categoryId:["",Validators.required]
+    })
+  }
+
+}
